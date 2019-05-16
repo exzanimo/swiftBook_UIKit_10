077bcad57cfc9b0e2f3cc82d33d6fadcef0c2b0b
Aleksandr Tarasov (exzanimo) <exzanimo@gmail.com>
2019-05-17 00:24:05 +0300

    Инициализация приложения, имитирующего первичный запуск с переключением приветствующих страниц.
    Добавлены: VC, PageVC, ContentVC, созданы классы и аутлеты для размещенных на ContentVC объектов.

---
+++ UI_Course10 (UIPageViewController)/PageVC.swift
@@ -0,0 +1,30 @@
+//
+//  PageVC.swift
+//  UI_Course10 (UIPageViewController)
+//
+//  Created by Александр Тарасов on 17/05/2019.
+//  Copyright © 2019 Aleksandr Tarasov. All rights reserved.
+//
+
+import UIKit
+
+class PageVC: UIPageViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
